14733b74b66dac4749d06e5f06491cba5e0a9dec
Tarashrita Ganguly <106442772+Tarashrita@users.noreply.github.com>
2023-05-25 19:11:26 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -21,10 +21,58 @@ function modelLoaded(){
     console.log("model loaded!");
 }
 
-function Speak(){
+function speak(){
     var synth =window.speechSynthesis;
     speak_data1="The first prediction is-"+prediction_1;
     speak_data2="The second prediction is-"+prediction_2;
     var utterThis= new SpeechSynthesisUtterance(speak_data1+speak_data2);
     synth.speak(utterThis);
+}
+
+function Check() {
+    img=document.getElementById('captured_img');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error,results) {
+if(error){
+    console.error(error);
+} else{
+    console.log(results);
+    document.getElementById("result_emotion_name").innerHTML=results[0].label;
+    document.getElementById("result_emotion_name2").innerHTML=results[1].label;
+    prediction_1=results[0].label;
+    prediction_2=results[1].label;
+    speak();
+    if(results[0].label==answer){
+        document.getElementById("update_emoji").innerHTML="&#129305;"
+    }
+    if(results[0].label==hello){
+        document.getElementById("update_emoji").innerHTML="&#9995;"
+    }
+    if(results[0].label==awesome){
+        document.getElementById("update_emoji").innerHTML="&#128076;"
+    }
+    if(results[0].label==good){
+        document.getElementById("update_emoji").innerHTML="&#128077;"
+    }
+    if(results[0].label==peace){
+        document.getElementById("update_emoji").innerHTML="&#9996;"
+    }
+    if(results[1].label==answer){
+        document.getElementById("update_emoji").innerHTML="&#129305;"
+    }
+    if(results[1].label==hello){
+        document.getElementById("update_emoji").innerHTML="&9995;"
+    }
+    if(results[1].label==awesome){
+        document.getElementById("update_emoji").innerHTML="&#128076;"
+    }
+    if(results[1].label==good){
+        document.getElementById("update_emoji").innerHTML="&#128077;"
+    }
+    if(results[1].label==peace){
+        document.getElementById("update_emoji").innerHTML="&#9996;"
+    }
+}
 }
